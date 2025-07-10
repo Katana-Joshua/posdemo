4f2adefde05d85c2c830dbdfc3013c22253fdcce
Katana-Joshua <katsjosh03@gmail.com>
2025-07-10 22:08:29 +0300

    12th

--- api/auth_signin.js
+++ api/auth_signin.js
@@ -34,6 +34,7 @@ export default async function handler(req, res) {
       }
     });
   } catch (error) {
+    console.error('AUTH_SIGNIN ERROR:', error);
     res.status(500).json({ error: 'Server error' });
   }
 } 
